d4579da64c253207c9cccfc2233cd88ede2160ae
gui326 <guilherme.326@hotmail.com>
2023-10-18 20:21:57 -0300

    Fix imports e adicionado ORM prisma

--- application/adapters/driver/api/startUp.ts
+++ application/adapters/driver/api/startUp.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import compression from "compression";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import * as compression from "compression";
 
 class StartUp {
   public app: express.Application;
