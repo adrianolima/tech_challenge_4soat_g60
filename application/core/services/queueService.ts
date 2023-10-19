a4f34b00d9cbc66b204a2fb8b9c6fc4974798265
andrauss <fernandoandrauss@hotmail.com>
2023-10-18 23:11:38 -0300

    Estrutura das interfaces do domínio

---
+++ application/core/services/queueService.ts
@@ -0,0 +1,11 @@
+/**
+ * Service to control the orders preparation queue
+ */
+class QueueService {
+    
+    
+    // método pra pegar os pedidos na fila de preparo
+    
+    // método para pegar os pedidos na fila de entrega
+    
+}
