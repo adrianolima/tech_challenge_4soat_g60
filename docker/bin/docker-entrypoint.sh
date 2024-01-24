d4f03f17766d6a09e6f08d67bb7d6d5e4137de4e
Adriano Lima <adrianoelima@gmail.com>
2024-01-24 18:40:46 -0300

    [4SOAT-G60] - Add hpa and fix services

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 # docker-entrypoint.sh for node.js
 
 echo "wait postgres db server"
-dockerize -wait tcp://127.0.0.1:5432 -timeout 30s
+dockerize -wait tcp://totem-postgres:5432 -timeout 30s
 
 RUN source /root/.bashrc
 
 echo "running migrations"
-./node_modules/.bin/prisma migrate dev --name init
+cd /var/www && ./node_modules/.bin/prisma migrate dev --name init
 
 echo "start node server"
-echo "node dist/adapters/driver/api/program.js"
-node dist/adapters/driver/api/program.js
+echo "node /var/www/dist/adapters/driver/api/program.js"
+node /var/www/dist/adapters/driver/api/program.js
